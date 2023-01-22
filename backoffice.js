b235233f7f868664dc90c10b0211c4f653f40db2
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2023-01-22 22:20:07 +0100

    so far

--- backoffice.js
+++ backoffice.js
@@ -5,8 +5,18 @@
 // }
 // })
 
-
 const url = "https://striveschool-api.herokuapp.com/api/movies"
+
+window.onload = () => {
+    getCategories();
+};
+
+const options = {                
+    headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQ0MTU4MjksImV4cCI6MTY3NTYyNTQyOX0.13kwGYawX6UMCIWdGW6s5yIZNWGHj6esfxBZPgCyGFQ"
+    }),
+};
 
 const postData = async(movie) => {
     try {
@@ -15,14 +25,13 @@ const postData = async(movie) => {
             body: JSON.stringify(movie),
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQyMDQzNTYsImV4cCI6MTY3NTQxMzk1Nn0.NkRa_FzwAWGfZF-jAqJaTZnqgFul4EWN_3k7fiDHSB0"
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQ0MTU4MjksImV4cCI6MTY3NTYyNTQyOX0.13kwGYawX6UMCIWdGW6s5yIZNWGHj6esfxBZPgCyGFQ"
             },
         });
     } catch(error) {
         console.log(error)
     }
 }
-
 
 const submitMovie = () => {
     let movie = {
@@ -33,3 +42,90 @@ const submitMovie = () => {
     };
     postData(movie);
 };
+
+const params = new URLSearchParams(location.search)
+const ID = params.get("id");
+const editContainer = document.querySelector("#editContainer");
+
+const allCategories = [];
+const getCategories = async () => {
+    try {
+        const res = await fetch(url, options);
+        const categories = await res.json();
+        categories.forEach((category) => {
+            allCategories.push(category);
+            getMovies(category);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const allMovies = [];
+const getMovies = async (category) => {
+    try {
+        const res = await fetch(url + "/" + category, options);
+        const movieData = await res.json();
+        movieData.forEach((movie) => {
+            allMovies.push(movie);
+        });
+        moviesBackOffice(movieData, category);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+const moviesBackOffice = (allMovies, category) => {
+    const check = document.querySelector(`#${category}Container`);
+    if (check === null) {
+        const tableContainer = document.createElement("div");
+        tableContainer.className = "container";
+        tableContainer.id = `${category}Container`;
+        tableContainer.innerHTML = "";
+        tableContainer.innerHTML = `
+        <div class="row">
+        <div class="col">
+        <table class="table table-info table-borderless text-align">
+            <thead class="thead-dark text-align">
+                <tr class="text-align">
+                <th scope="Image">Images</th>
+                <th scope="col">Name</th>
+                <th scope="col">Genre</th>
+                <th scope="col">ID</th>
+                <th scope="row">Edit/Delete</th>
+                </tr>
+            </thead>
+            <tbody ></tbody>
+                </table>
+        </div>
+      </div>`;
+        editContainer.appendChild(tableContainer)
+
+        const tableBody = document.querySelector(`#${category}Container tbody`);
+
+        const moviesInside = allMovies.map(({ name, imageUrl, _id}) => {
+            return `<tr>
+            <td><img src="${imageUrl}" style="object-fit:cover; width:60px; height: 60px"></td>
+            <td>${name}</td>
+            <td>${category}</td>
+            <td>${_id}</td>
+            
+            <td>
+            
+            
+            <div>
+                <button type="button" class="btn btn-info" onclick="editMovie()">EDIT</button>
+                <button type="button" class="btn btn-danger" onclick="deleteMovie('${_id}')">DELETE</button>         
+            </div>
+            </td>
+        </tr>`;
+
+        })
+        .join("");
+        tableBody.innerHTML = moviesInside;
+    }
+}
+
--- front.js
+++ front.js
@@ -9,7 +9,7 @@
 
 
 headers: {
-  "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQyMDQzNTYsImV4cCI6MTY3NTQxMzk1Nn0.NkRa_FzwAWGfZF-jAqJaTZnqgFul4EWN_3k7fiDHSB0"
+  "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQ0MTU4MjksImV4cCI6MTY3NTYyNTQyOX0.13kwGYawX6UMCIWdGW6s5yIZNWGHj6esfxBZPgCyGFQ"
   } 
 
 
@@ -21,38 +21,177 @@ headers: {
 // }
 // })
 
+/*
+ window.onload = () => {
+  getGenres();
+};
 
-const url = "https://striveschool-api.herokuapp.com/api/movies"
+const urlParams = new URLSearchParams(location.search);
+const ID = urlParams.get("id");
+console.log(ID);
 
-const getData = async () => {
+
+const url = "https://striveschool-api.herokuapp.com/api/movies";
+
+
+
+const options = {                
+    headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQ0MTU4MjksImV4cCI6MTY3NTYyNTQyOX0.13kwGYawX6UMCIWdGW6s5yIZNWGHj6esfxBZPgCyGFQ"
+    }),
+};
+
+
+
+const allGenres = [];
+const getGenres = async () => {
     try {
-        let res = await fetch(url, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQyMDQzNTYsImV4cCI6MTY3NTQxMzk1Nn0.NkRa_FzwAWGfZF-jAqJaTZnqgFul4EWN_3k7fiDHSB0"
-                },
+        const response = await fetch(url, options);
+        const genres = await response.json();
+        genres.forEach((genre) => {
+            allGenres.push(genre);
+            getMovies(genre);
         });
-        if (res.ok) {
-        let data = res.json()
-        console.log(data);
-        displayMovies(data)
+    } catch (error) {
+        console.log(error);
     }
-} catch (error) {
-    console.log(error);
-}
-}
+};
 
-getData()
 
-const displayMovies = (data) => {
-  let movieRowNode = document.getElementById('row');
-  console.log(movieRowNode)
-  data.forEach((singleMovie) => {
-      singleMovie.innerHTML += `
-      <div class="col-md-2">
-      <h2> ${singleMovie.category} </h2>
-      </div>
-      `
-  })
-}
+ 
+const getMovies = async (genre) => {
+    try {
+        const response = await fetch(url + "/" + genre, options);
+        const data = await response.json();
+        displayMovies(data, genre);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+const comedyContainer = document.querySelector("#comedyContainer")
+
+const actionContainer = document.querySelector("#actionContainer")
+
+const horrorContainer = document.querySelector("#horrorContainer")
+
+const allContainer = document.querySelector("#allContainer")
+
+
+const allMovies = []
+const displayMovies = (allMovies, genre) => {
+    let theMovies = null;
+    switch (genre) {
+        case "comedy":
+            spaceForMovies = comedyContainer;
+            break;
+        case "action":
+            spaceForMovies = actionContainer;
+            break;
+        case "horror":
+            spaceForMovies = horrorContainer;
+            break;
+        }
+
+        if (theMovies !== null) {
+            moviesHTML = allMovies
+            .map(({ name, description, imageUrl }) => {
+                return `<div class="movie-card card col-sm-6 col-md-4 col-lg-3 col-xl-2 mx-2 my-2" style="width: 18rem;">
+                <img src="${imageUrl}" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-title">${name}</p>
+                  <div class="dropdown-divider"></div>
+                  <p class="card-text">${description}</p>
+                </div>
+              </div>`;
+            })
+            .join("");
+            spaceForMovies.innerHTML = moviesHTML;
+        }
+};
+*/
+
+const url = "https://striveschool-api.herokuapp.com/api/movies";
+
+const options = {                
+    headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzA5OWU3MzczODAwMTUzNzQzNzIiLCJpYXQiOjE2NzQ0MTU4MjksImV4cCI6MTY3NTYyNTQyOX0.13kwGYawX6UMCIWdGW6s5yIZNWGHj6esfxBZPgCyGFQ"
+    }),
+};
+
+window.onload = () => {
+    getGenres();
+};
+
+const allGenres = [];
+const getGenres = async () => {
+    // GET Method
+    try {
+        const response = await fetch(url, options);
+        const genres = await response.json();
+        genres.forEach((genre) => {
+            allGenres.push(genre);
+            getMovies(genre);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getMovies = async (genre) => {
+    try {
+        const response = await fetch(url + "/" + genre, options);
+        const movieNode = await response.json();
+        renderMovies(movieNode, genre);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const urlParams = new URLSearchParams(location.search);
+const ID = urlParams.get("id");
+console.log(ID);
+
+const comedyContainer = document.querySelector("#comedyContainer");
+
+const actionContainer = document.querySelector("#actionContainer");
+
+const horrorMoviesContainer = document.querySelector("#horrorMoviesContainer");
+
+
+const allMovies = []
+const renderMovies = (allMovies, genre) => {
+    let moviesContainer = null;
+    switch (genre) {
+        case "comedy":
+            moviesContainer = comedyContainer;
+            break;
+        case "action":
+            moviesContainer = actionContainer;
+            break;
+        case "horror":
+            moviesContainer = horrorMoviesContainer;
+            break;
+        }
+        if (moviesContainer !== null) {
+            moviesInside = allMovies
+            .map(({ name, description, imageUrl }) => {
+                return `<div class="movie-card card" style="width: 10rem;">
+                <img src="${imageUrl}" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-title text-secondary">${name}</p>
+                  <div class="dropdown-divider"></div>
+                  <p class="card-text text-secondary">${description}</p>
+                </div>
+              </div>`;
+            })
+            .join("");
+            moviesContainer.innerHTML = moviesInside;
+        }
+};
+
 
